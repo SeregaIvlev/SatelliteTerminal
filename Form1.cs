65a56ad7d8d7b7a72554b6d26e41356515ccb275
TheForceFire <104731245+TheForceFire@users.noreply.github.com>
2022-05-14 19:38:11 +0400

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -5,16 +5,41 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
-
+using System.IO.Ports;
 namespace SatelliteTerminal
 {
     public partial class Form1 : Form
     {
+        SerialPort port = new SerialPort();
+
         public Form1()
         {
             InitializeComponent();
+
+            if (port.IsOpen)
+            {
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            }
+
+            
+        }
+
+        [STAThread]
+
+        private void Receive()
+        {
+            byte[] Buff;
+            int k = 0;
+            do
+            {
+                Thread.Sleep(50);
+                Buff = new byte[port.BytesToRead];
+                port.Read(Buff, 0, port.BytesToRead);
+                k++;
+            }
+            while (Buff.Length == 0 && k < 40);
         }
 
         private void label8_Click(object sender, EventArgs e)
@@ -50,6 +75,49 @@ namespace SatelliteTerminal
         private void radioButton4_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+        
+        
+            
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine("Выберите порт:");
+            listBox1.Items.AddRange(ports);
+
+        }
+        private void button11_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                // настройки порта
+                port.PortName = listBox1.SelectedItem.ToString();
+                port.BaudRate = 115200;
+                port.DataBits = 8;
+                port.Parity = System.IO.Ports.Parity.Even;
+                port.StopBits = System.IO.Ports.StopBits.One;
+                port.ReadTimeout = 2000;
+                port.WriteTimeout = 1000;
+                port.Open();
+                byte[] a = { 0x57 };
+                port.Write(a, 0, a.Length);
+
+                
+                //port.Close();
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine("ERROR: невозможно открыть порт:" + a.ToString());
+                return;
+            }
+
+        }
+
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            tbOutput.Text += port.ReadExisting();
         }
     }
 }
